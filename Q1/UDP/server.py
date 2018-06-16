53592763b6b97b87ffecff144132ed2c380e5aee
Igor Moura <imp2@cin.ufpe.br>
2018-06-16 19:59:49 -0300

    fixed udp

--- Q1/UDP/server.py
+++ Q1/UDP/server.py
@@ -1,23 +1,17 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-
-# Author: David Manouchehri <manouchehri@protonmail.com>
-# This script will always echo back data on the UDP port of your choice.
-# Useful if you want nmap to report a UDP port as "open" instead of "open|filtered" on a standard scan.
-# Works with both Python 2 & 3.
+#!/usr/bin/env python3
 
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = '0.0.0.0'
-server_port = 31337
+serverAddr = '0.0.0.0'
+serverPort = 8080
 
-server = (server_address, server_port)
-sock.bind(server)
-print("Listening on " + server_address + ":" + str(server_port))
+server = (serverAddr, serverPort)
+mySocket.bind(server)
+print("Listening on " + serverAddr + ":" + str(serverPort))
 
 while True:
-	payload, client_address = sock.recvfrom(1)
-	print("Echoing data back to " + str(client_address))
-	sent = sock.sendto(payload, client_address)
+	payload, clientAddr = mySocket.recvfrom(1024)
+	print("Echoing data \"" + str(payload.decode('utf-8')) +"\" back to " + str(clientAddr))
+	sent = mySocket.sendto(payload, clientAddr)
